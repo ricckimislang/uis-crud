36d4529f870f2235b23f15b66ee7cb108a8feae0
riccki mislang <dragonmislang9@gmail.com>
2025-05-24 18:56:49 +0800

    rolled back innoDB table from users with sessions to avoid conflict

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
         Schema::create('sessions', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index()->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
